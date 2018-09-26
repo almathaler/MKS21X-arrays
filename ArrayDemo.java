c92008e01ee141d44f276da137209df2524ff587
almathaler <athaler00@stuy.edu>
2018-09-26 19:31:04 -0400

    got 0b

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -1,13 +1,34 @@
 class ArrayDemo {
   public static void main(String[] args) {
-    System.out.print("First printArray; should print \"1 2 3 4\" ");
+    //testing printArray
+    System.out.print("1D printArray should print \"1 2 3 4\" ");
     int[] ary = {1, 2, 3, 4};
     printArray(ary);
-    System.out.println("");
+    //System.out.println("");
+    //testing printArray for 2D arrays
+    System.out.print("2D printArray should print \"1 2 3 4 \n 2 4 6 8 \n 1 3 5 7\" \n");
+    int[][] ary2 = new int[][] {
+      {1, 2, 3, 4},
+      {2, 4, 6, 8},
+      {1, 3, 5, 7},
+    };
+    printArray(ary2);
+    //System.out.println("");
   }
   public static void printArray (int[] ary) {
     for (int i = 0; i<ary.length; i++) {
-      System.out.print(ary[i] + " ");
+      if (i == ary.length-1) {
+        System.out.print(ary[i] + "\n");
+      }
+      else {
+        System.out.print(ary[i] + " ");
+      }
+    }
+    //System.out.println("");
+  }
+  public static void printArray(int[][] ary){
+    for (int i = 0; i< ary.length; i++) {
+      printArray(ary[i]);
     }
   }
 }

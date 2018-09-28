d25ccd37863537ce0991a0bf7eb5481388f30162
almathaler <athaler00@stuy.edu>
2018-09-27 22:14:21 -0400

    finished

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -46,6 +46,15 @@ class ArrayDemo {
     System.out.println("Now let's test 2a. Let's give it ary2 and see what is returned");
     fill2D(ary2);
     printArray(ary2);
+    System.out.println("Finally, let's test 2b. Given an array of 3 rows with 5, 6 and 3 items in length 1D arrays");
+    System.out.println("We should see:\n {1, 1, 3, 1, 1}\n {1, 3, 3, 1, 1, 1}\n {1, 1, 1,}");
+    int[][] ary7 = new int[][] {
+                              {2, 3, -4, 3, 1, },
+                              {2, -1, -65, 43, 223, 456},
+                              {3, 4, 2},
+    };
+    printArray(fill2DCopy(ary7));
+    System.out.println("Thank you for viewing my homework!");
   }
   public static void printArray (int[] ary) {
     for (int i = 0; i<ary.length; i++) {
@@ -95,5 +104,19 @@ class ArrayDemo {
     }
   //  System.out.println(printArray(vals));
   }
-
+  public static int[][] fill2DCopy(int[][] vals){
+    int[][] copy = new int[vals.length][];
+    for (int r = 0; r<vals.length; r++) {
+      copy[r] = new int[vals[r].length];
+      for (int c = 0; c<vals[r].length; c++) {
+        if (vals[r][c] < 0) {
+          copy[r][c] = 3;
+        }
+        else {
+          copy[r][c] = 1;
+        }
+      }
+    }
+    return copy;
+  }
 }
